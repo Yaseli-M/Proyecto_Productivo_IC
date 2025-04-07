208992939cea67df15aa4615344a13c0a6126e8e
Yaseli <yas97mendez@gamil.com>
2025-04-06 23:09:52 -0500

    Actualizando los datos

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ import seaborn as sns
 import numpy as np
 from supabase import create_client, Client
 
-# Configura Supabase
-url = "https://igdyolghmogpjmjlpzgs.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+# Configura Supabase con secretos
+url = st.secrets["https://igdyolghmogpjmjlpzgs.supabase.co"]
+key = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnZHlvbGdobW9ncGptamxwemdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5OTQwNTUsImV4cCI6MjA1OTU3MDA1NX0.Z7ek0HjEOkZ-4heMSzrfMCDoUBg9M7mpS6vE504bgPc"]
 supabase: Client = create_client(url, key)
 
 # Título
 st.title("Predicción de Riesgo Crediticio")
 
-# Carga los datos desde Supabase
+# Cargar datos desde Supabase
 @st.cache_data
 def cargar_datos():
     response = supabase.table("credit_risk_data").select("*").execute()
